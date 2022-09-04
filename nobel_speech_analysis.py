5b11317e9e11b6e1fdda6468b18c91e51630a18c
Eloragh Espie <101366130+eloragh@users.noreply.github.com>
2022-09-03 23:50:14 -0500

    update

--- nobel_speech_analysis.py
+++ nobel_speech_analysis.py
@@ -30,6 +30,9 @@ def create_wordlist(file):
 
     vocab_list = []
 
+    #iterate through each file
+    #uses regex to check for more complicated issues in the data
+
     for file in files:
 
         file1 = open(file, encoding = "utf8")
@@ -42,14 +45,22 @@ def create_wordlist(file):
         lst = file5.lower().split(" ")
 
         for word in lst:
-            if word.startswith("\'"):
-                pass
-            elif word not in stops:
+
+            #check that the words are not stopwords
+            #stopwords are the most common words in a language
+            #in English it's "the", "and", "a", "then", "there", etc.
+            #tldr; not relevant to the analysis
+
+            if word not in stops:
                 vocab_list.append(word)
+
+        #use the Counter method to create key:value pairs
 
         most_frequent = col.Counter(vocab_list)
     
     most_frequent_words = []
+
+    #iterate through the counter to form a list so it can be sorted by most frequent words
 
     for key in most_frequent:
         if most_frequent[key] > 2:
@@ -64,6 +75,8 @@ def create_wordlist(file):
 
 def main(file):
 
+    #ask the user how many words they would like to see
+
     num = int(input("How many words would you like to see?: "))
 
     words = create_wordlist(files)
@@ -71,6 +84,5 @@ def main(file):
     print(words[0:num-1])
 
 main(files)
-
 
 
